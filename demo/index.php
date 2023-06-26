d8994bb8f53b26c229e39a542c38c288ea2a9037
sonnenbergmarc <msonnenberg@live.com>
2023-06-25 21:58:57 -0600

    Basic changes to index.php

---
+++ demo/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $greeting = "Hello";
+            echo $greeting . " " . "Everybody!";
+        ?>
+    </h1>
+</body>
+</html>
