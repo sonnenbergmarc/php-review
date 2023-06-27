9d84c5df3046dc6ac8c8efde33a191ea81b44c00
sonnenbergmarc <msonnenberg@live.com>
2023-06-26 20:58:34 -0600

    Arrays

--- demo/index.php
+++ demo/index.php
@@ -5,29 +5,19 @@
     <title>Demo</title>
 </head>
 <body>
-    <!-- <h1>Recommended Books</h1> -->
+    <h1>Recommended Books</h1>
 
     <?php
-        $name = "Dark Matter";
-        $read = TRUE;
-        if ($read) {
-            $message = "You have read \"$name\"";
-        } else {
-            $message = "You have NOT read \"$name\"";
-        };
-        // $books = [
-        //     "Do Androids Dream of Electric Sheep",
-        //     "The Langoliers",
-        //     "Hail Mary"
-        // ];
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
-    <!-- <ul>
-        <php foreach ($books as $book) {
-            //
-        } ?>
-    </ul> -->
-    <h1>
-        <?= $message; ?>
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book; ?></li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
